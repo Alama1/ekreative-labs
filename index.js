1eab0fd64e544dee3945136b6e7b39b933dbe04e
Maxim Kuzhdenko <oldpaladin7@gmail.com>
2023-11-30 18:02:16 +0200

    Second page 'about' added and some light changes

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const details = document.querySelectorAll("details");
 
+details.forEach((targetDetail) => {
+    targetDetail.addEventListener("click", () => {
+        details.forEach((detail) => {
+            if (detail !== targetDetail) {
+                detail.removeAttribute("open");
+            }
+        });
+    });
+});
